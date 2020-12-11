87c16bf5e65167e0ceac47a49d79b0357b5d19f5
Kavita Jadhav <kavita.jadhav1109@gmail.com>
2020-12-11 02:07:22 -0800

    Update HTTPURLConnectionUtil.java

--- HTTPURLConnectionUtil.java
+++ HTTPURLConnectionUtil.java
@@ -14,6 +14,20 @@ import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+/*
+A simple java class which can be used to make GET and POST requests extremely simple and reliable to be used in Android Applications.
+
+GET Request String getURL = "https://www.somewebsite.com/api/param1=value1&param2=value2"; 
+String getResponse = new HTTPURLConnection().GET(getURL);
+
+POST Request String postURL = "https://www.somewebsite.com/api"; 
+String postResponse = new HTTPURLConnection().POST(postURL);
+
+POST Request with parameters String postURL = "https://www.somewebsite.com/api"; 
+HashMap<String, String> parameters = new HashMap<String, String>(); 
+parameters.put("param1", "value1"); parameters.put("param2", "value2"); 
+String postResponse = new HTTPURLConnection().POST(postURL, parameters);
+*/
 
 public class HTTPURLConnectionUtil {
     private String response = "";
